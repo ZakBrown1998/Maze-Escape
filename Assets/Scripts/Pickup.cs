269b4a868459bf7b26916e1709398968175a6b7a
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2018-10-05 14:57:15 +0100

    Added Score, Textures and Camera behaviour

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public Score scoreTracker;
+
+    public int scoreValue = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Only do stuff if it's the player we've hit
+            if (collision.GetComponent<Player>())
+        {
+            //Updating the score based on how much this item is worth
+            scoreTracker.ChangeValue(scoreValue);
+            Destroy(gameObject);
+        
+                }
+    }
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //public variables visible in Unity
+    public TextMesh displayText;
+
+    //Private variables cna't be touched by other scripts
+    private int currentValue = 0;
+
+
+
+    // Update both the data value AND the visual display
+    public void ChangeValue(int _toChange)
+    {
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+
+
+}
+
+    //Reset the score to 0
+
+    public void ResetScore()
+    {
+        currentValue = 0;
+        displayText.text = currentValue.ToString();
+    }
+}
